e1bc904210114a7329564ce62d148a6db6dab555
peng <ppp8300885@gmail.com>
2016-10-20 02:12:02 +0800

    hi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ApplicationController
 
   def index
+    if params[:chart_type]=='heatmap'
+      render 'home/heatmap'
+    end
+
   end
 
   def get_collection
-    houses=House.where.not('houses.latitude' => nil).where.not('houses.longitude' => nil)
+    houses=House.where.not('houses.latitude' => nil).where.not('houses.longitude' => nil).where('houses.distance < 40000')
     respond_to do |format|
       format.json { render :json => houses }
     end
